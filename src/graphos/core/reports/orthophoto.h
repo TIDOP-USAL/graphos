cb91e26a05cfac0bd1f33ed12482371ee4f48090
Esteban <estebanrdo@usal.es>
2024-11-06 18:24:25 +0100

    Missing files added

---
+++ src/graphos/core/reports/orthophoto.h
@@ -0,0 +1,44 @@
+/************************************************************************
+ *                                                                      *
+ *  Copyright 2016 by Tidop Research Group <daguilera@usal.es>          *
+ *                                                                      *
+ * This file is part of GRAPHOS - inteGRAted PHOtogrammetric Suite.     *
+ *                                                                      *
+ * GRAPHOS - inteGRAted PHOtogrammetric Suite is free software: you can *
+ * redistribute it and/or modify it under the terms of the GNU General  *
+ * Public License as published by the Free Software Foundation, either  *
+ * version 3 of the License, or (at your option) any later version.     *
+ *                                                                      *
+ * GRAPHOS - inteGRAted PHOtogrammetric Suite is distributed in the     *
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without even  *
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR  *
+ * PURPOSE.  See the GNU General Public License for more details.       *
+ *                                                                      *
+ * You should have received a copy of the GNU General Public License    *
+ * along with Graphos.  If not, see <http://www.gnu.org/licenses/>.     *
+ *                                                                      *
+ * https://spdx.org/licenses/GPL-3.0-or-later.html                      *
+ *                                                                      *
+ ************************************************************************/
+
+#ifndef GRAPHOS_CORE_ORTHOPHOTO_REPORT_H
+#define GRAPHOS_CORE_ORTHOPHOTO_REPORT_H
+
+namespace graphos
+{
+
+struct OrthophotoReport
+{
+    double time = 0.0;
+	double gsd = 0.0;
+	
+    bool isEmpty() const
+    {
+        return time == 0. && gsd == 0.;
+    }
+};
+
+
+} // namespace graphos
+
+#endif // GRAPHOS_CORE_ORTHOPHOTO_REPORT_H
